18db59f6f11008b001aa905de3bfee420f57bc11
MStewart0218 <https://github.com/MStewart0218/Election_Analysis.git>
2022-01-23 18:13:17 -0600

    Updated PyPoll.py file.

--- Resources/PyPoll.py
+++ Resources/PyPoll.py
@@ -1,4 +1,5 @@
 #The data we need to retrieve. C:\Users\Marga\Election_Analysis\Resources\election_results.csv
+#Add our dependecies.
 import csv
 import os
 
@@ -8,27 +9,35 @@ file_to_load = 'election_results.csv'
 # Assign/ Create a filename variable to a direct or indirect path to the file.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+# 1. Initialize a total vote counter
+total_votes = 0
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
 
 # To do: perform analysis.
-    #print(str(election_data))_IMPORTANT TO USE LATER IN CODE
+    #print(str(election_data))-IMPORTANT TO USE LATER IN CODE
 
     file_reader = csv.reader(election_data)
 
-# Print the header row.
+    # Read the header row.
     headers = next(file_reader)
-    print(headers)    
+        #print(headers)    
 
 # Print each row in the CSV file.
-    #for row in file_reader:
-        #print(row[0])
+    for row in file_reader:
+        # 2. Add to the total vote count.
+        total_votes += 1
+
+
+# 3. Print the total votes.
+print(total_votes)
 
 
 # Using the open() function with the "w" mode we will write data to the file.
-with open(file_to_save, "w") as txt_file:
+#with open(file_to_save, "w") as txt_file:
 #write some data to the file.
-    txt_file.write(str("Counties in the Election\n------------------------\nArapahoe\nDenver\nJefferson"))
+    #txt_file.write(str("Counties in the Election\n------------------------\nArapahoe\nDenver\nJefferson"))
 
 
 #1. The total number of votes cast 
